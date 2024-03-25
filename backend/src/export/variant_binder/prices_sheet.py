950f2584f3f78be8a5afee71c454bd531326ea86
Hassan Wahba <hassan.wahba@volvocars.com>
2024-03-25 18:13:09 +0100

    Refactor prices_sheet.py to improve code readability and performance

--- backend/src/export/variant_binder/prices_sheet.py
+++ backend/src/export/variant_binder/prices_sheet.py
@@ -5,8 +5,7 @@ from openpyxl.utils import get_column_letter
 import pandas as pd
 
 from src.database.db_operations import DBOperations
-from src.utils.db_utils import filter_df_by_timestamp
-from src.utils.db_utils import format_float_string
+from src.utils.db_utils import filter_df_by_timestamp, format_float_string
 
 
 black_side = Side(color='000000', border_style='thin')
@@ -32,29 +31,27 @@ def get_sheet(ws, df_valid_pnos, df_sales_versions, title, time, df_engines_type
     """
     df_price, df_gb, df_en = fetch_vb_price_data(country, df_valid_pnos, time)
     
-    
-    prepare_sheet(ws, title)
+    #get length of the code column, where code is an array and we want the max length of any array in the Code column
+    max_code_length = df_engines_types['Code'].apply(lambda x: len(x)).max()
+    prepare_sheet(ws, title, max_code_length)
     curr_row = 3
     for _, row in df_engines_types.iterrows():
         type = row['EngineType']
         group = df_price[df_price['Engine'].isin(row['Code'])]
-
-        df_group_sv = pd.DataFrame()
-        df_group_sv['Code'] = group['SalesVersion'].drop_duplicates()
-        df_group_sv['MarketText'] = df_group_sv['Code'].apply(lambda x: df_sales_versions[df_sales_versions['SalesVersion'] == x].iloc[0]['SalesVersionName'])
         
         df_group_gb = pd.DataFrame()
         df_group_gb['Code'] = group['Gearbox'].drop_duplicates()
-        df_group_gb['MarketText'] = df_group_gb['Code'].apply(lambda x: df_gb[df_gb['Code'] == x].iloc[0]['MarketText'])
+        df_group_gb['MarketText'] = df_group_gb['Code'].apply(lambda x: df_gb[df_gb['Code'] == x].iloc[0]['CustomName'])
         
         df_group_en = pd.DataFrame()
         df_group_en['Code'] = group['Engine'].drop_duplicates()
-        df_group_en['MarketText'] = df_group_en['Code'].apply(lambda x: df_en[df_en['Code'] == x].iloc[0]['MarketText'])
+        df_group_en['MarketText'] = df_group_en['Code'].apply(lambda x: df_en[df_en['Code'] == x].iloc[0]['CustomName'])
+        df_group_en['Performance'] = df_group_en['Code'].apply(lambda x: df_en[df_en['Code'] == x].iloc[0]['Performance'])
         curr_row = insert_engines_type_title(ws, type, curr_row)
-        curr_row = insert_table(ws, group, df_group_sv, df_group_gb, df_group_en, curr_row)
+        curr_row = insert_table(ws, group, df_sales_versions, df_group_gb, df_group_en, curr_row)
         curr_row += 1
 
-def prepare_sheet(ws, title):
+def prepare_sheet(ws, title, max_code_length):
     ws.sheet_view.showGridLines = False
     ws.sheet_view.headingsVisible = False
     ws.freeze_panes = ws['A2']
@@ -63,11 +60,13 @@ def prepare_sheet(ws, title):
         ws.row_dimensions[i].height = 12
     ws.column_dimensions['A'].width = 45
     
-    create_title(ws, f'Volvo {title} - Preise')
+    create_title(ws, f'Volvo {title} - Preise', max_code_length)
 
-def create_title(ws, title):
+def create_title(ws, title, max_code_length):
     ws.row_dimensions[1].height = 40
-    ws.merge_cells('A1:J1')
+    # get letter from number
+    letter = get_column_letter(max_code_length + 1)
+    ws.merge_cells(f'A1:{letter}1')
     ws.cell(row=1, column=1, value=title)
     ws.cell(row=1, column=1).alignment = Alignment(horizontal='left', vertical='center')
     ws.cell(row=1, column=1).fill = PatternFill(start_color='000080', end_color='000080', fill_type='solid')
@@ -84,7 +83,7 @@ def insert_table(ws, df_price, df_sv, df_gb, df_en, curr_row):
     mwst_row = curr_row
     df = df_price.copy()
     prepend_string = df.Model.iloc[0] + ' SV '
-    df_sv['FullCode'] = prepend_string + df_sv['Code']
+    df_sv['FullCode'] = prepend_string + df_sv['SalesVersion']
     insert_meta_column(ws, curr_row, df_sv)
     curr_row += 1
     ws.row_dimensions[curr_row].height = 24
@@ -123,7 +122,7 @@ def insert_mwst_line(ws, curr_row, curr_col):
 
 def get_price_column(group, df_sv):
     prices = [] 
-    for sv in df_sv['Code']:
+    for sv in df_sv['SalesVersion']:
         entry = group[group['SalesVersion'] == sv]
         if entry.empty:
             prices.append('-')
@@ -148,7 +147,7 @@ def insert_meta_column(ws, curr_row, df_sales_version):
         ws.cell(row=curr_row, column=1).alignment = Alignment(horizontal='left', vertical='center')
         ws.cell(row=curr_row, column=1).border = blr_border
         
-    alternating_list = [item for pair in zip(df_sales_version['MarketText'], df_sales_version['FullCode']) for item in pair]
+    alternating_list = [item for pair in zip(df_sales_version['SalesVersionName'], df_sales_version['FullCode']) for item in pair]
     for i, field in enumerate(alternating_list):
         curr_row += 1
         ws.cell(row=curr_row, column=1, value=field)
@@ -194,13 +193,13 @@ def fetch_vb_price_data(country, df_valid_pnos, time):
     df_gb = DBOperations.instance.get_table_df(DBOperations.instance.config.get('TABLES', 'G'), conditions=[f'CountryCode = {country}'])
     df_gb = df_gb[df_gb['Code'].isin(df_price['Gearbox'].tolist())]
     df_gb = filter_df_by_timestamp(df_gb, time)
-    df_gb = df_gb[['Code', 'MarketText']]
-    df_gb = df_gb.drop_duplicates(subset=['Code'], keep='last')
+    df_gb['CustomName'] = df_gb['CustomName'].combine_first(df_gb['MarketText'])
+    df_gb = df_gb[['Code', 'CustomName']]
 
     df_en = DBOperations.instance.get_table_df(DBOperations.instance.config.get('TABLES', 'En'), conditions=[f'CountryCode = {country}'])
     df_en = df_en[df_en['Code'].isin(df_price['Engine'].tolist())]
     df_en = filter_df_by_timestamp(df_en, time)
-    df_en = df_en[['Code', 'MarketText']]
-    df_en = df_en.drop_duplicates(subset=['Code'], keep='last')
+    df_en['CustomName'] = df_en['CustomName'].combine_first(df_en['MarketText'])
+    df_en = df_en[['Code', 'CustomName', 'Performance']]
 
     return df_price, df_gb, df_en
