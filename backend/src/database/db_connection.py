0a76f5a4bf90c2c53eb2074cd099c055ba8b3cf8
Hassan Wahba <hassan.wahba@volvocars.com>
2024-03-25 09:29:07 +0100

    Refactor get_db_connection method to include retry logic

--- backend/src/database/db_connection.py
+++ backend/src/database/db_connection.py
@@ -1,6 +1,7 @@
 import os
 import pyodbc
 import logging
+from time import sleep
 
 class DatabaseConnection:
     logger = logging.getLogger(__name__)
@@ -18,24 +19,18 @@ class DatabaseConnection:
             raise e
 
     @classmethod
-    def get_db_connection(self):
+    def get_db_connection(self, max_attempts=3):
         self.logger.debug('Starting database connection')
-        try:
-            if self.connection is None or self.connection.closed:
-                connection_string = os.environ.get('DB_CONNECTION_STRING')
-                self.connection = pyodbc.connect(connection_string)
-            return self.connection
-        except pyodbc.Error as e:
-            self.logger.error('Failed to start database connection', exc_info=True)
-            raise e
+        for attempt in range(max_attempts):
+            try:
+                if self.connection is None or self.connection.closed:
+                    connection_string = os.environ.get('DB_CONNECTION_STRING')
+                    self.connection = pyodbc.connect(connection_string)
+                return self.connection
+            except pyodbc.Error as e:
+                self.logger.error(f'Failed to start database connection, attempt {attempt + 1}', exc_info=True)
+                if attempt + 1 == max_attempts:
+                    raise e
+                else:
+                    sleep(1)
         
-    @classmethod
-    def create_connection(self, connection_string):
-        self.logger.debug('Starting database connection')
-        try:
-            if self.connection is None or self.connection.closed:
-                self.connection = pyodbc.connect(connection_string)
-            return self.connection
-        except pyodbc.Error as e:
-            self.logger.error('Failed to start database connection', exc_info=True)
-            raise e
