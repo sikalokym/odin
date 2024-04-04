47266a1ada854e1d14d21b8aec8e009e509e47e3
Hassan Wahba <hassan.wahba@volvocars.com>
2024-04-04 09:33:37 +0200

    Update package.json and add server.js for serving static files

---
+++ frontend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+
+const app = express();
+
+// Serve the static files in the dist directory
+app.use(serveStatic(path.join(__dirname, 'dist')));
+
+// Catch all routes and redirect to the index.html
+app.get('*', function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+// Choose the port and start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
