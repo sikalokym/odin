8a14b6fe5a56ff0421f0dfcb8a2662b9bf045cd5
Hassan Wahba <hassan.wahba@volvocars.com>
2024-04-05 19:30:12 +0200

    Add format to the sheet

--- backend/src/export/variant_binder/upholstery_colors_sheet.py
+++ backend/src/export/variant_binder/upholstery_colors_sheet.py
@@ -1,10 +1,17 @@
 from src.database.db_operations import DBOperations
-from openpyxl.styles import PatternFill, Border, Side, Alignment, Font
-from openpyxl.utils import get_column_letter
-
+from src.export.variant_binder import options_sheet
 from src.utils.db_utils import filter_df_by_timestamp, format_float_string
-from openpyxl.styles.borders import Border
-from openpyxl.styles import Side
+
+from openpyxl.styles import PatternFill, Border, Alignment, Border, Side, Font
+import pandas as pd
+
+cell_values = {
+    'B': '•',
+    'U': 'o',
+    '=': 'o',
+    'C': 'o',
+    '': '-'
+}
 
 all_border = Border(top=Side(style='thin', color='000000'),
                     bottom=Side(style='thin', color='000000'),
@@ -26,180 +33,88 @@ def get_sheet(ws, sales_versions, title, time):
     Returns:
         None
     """
-    df_colors = fetch_color_data(sales_versions, time)
     df_upholstery = fetch_upholstery_data(sales_versions, time)
+    df_colors = fetch_color_data(sales_versions, time)
 
-    prepare_sheet(ws, sales_versions, title)
-
-    # Write the data to the worksheet
-    
-    for _, row in df_upholstery.iterrows():
-        prices = row['Price'].split('/')
-        if len(prices) > 1:
-            ws.append([row['Code'], row['CustomName'], prices[0]] + [row[sv] for sv in sales_versions['SalesVersion']])
-            ws.cell(row=ws.max_row, column=3).alignment = Alignment(horizontal='center', vertical='bottom')
-            ws.append(['', '', prices[1]] + [row[sv] for sv in sales_versions['SalesVersion']]) 
-            ws.cell(row=ws.max_row, column=3).alignment = Alignment(horizontal='center', vertical='top')
-            
-
-        # format the cells
-        for cell in ws[len(ws["A"])]:
-            if row['Price'] =='Pack Only':
-                cell.fill = PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='solid')
-            cell.border = all_border
-    
+    insert_table(ws, sales_versions, title + ' - Polster', df_upholstery)
     ws.append([])
-    ws.append([])
-    curr_row = len(ws["A"]) + 2
-    ws.merge_cells(f'A{curr_row}:B{curr_row}')
-    ws[f'A{curr_row}'] = 'Außenfarben'
-    ws[f'C{curr_row}'] = 'EUR inkl. 19 % MwSt.\n EUR ohne MwSt.'
-    for idx, value in enumerate(sales_versions['SalesVersionName'], start=4):
-        ws.cell(row=curr_row, column=idx, value=value)
-        
-    # Formatting of first row
-    ws[f'A{curr_row}'].font = Font(name='Arial', size=16, bold=True, color="FFFFFF")
-    ws[f'A{curr_row}'].alignment = Alignment(horizontal='left', vertical='center')
+    insert_table(ws, sales_versions, title + ' - Außenfarben', df_colors)
     
-    for col in range(3, 20):
-        cell = ws.cell(row=curr_row, column=col)
-        cell.font = Font(name='Arial', size=10, bold=True, color='FFFFFF')
-        cell.alignment = Alignment(horizontal='center', vertical='center',wrap_text=True)
-    max_c = ws.max_column
-    for col in range(1,max_c+1):
-        cell = ws.cell(row=curr_row, column=col)
-        cell.fill = fill
-        
-    for _, row in df_colors.iterrows():
-        ws.append([row['Code'], row['CustomName'], row['Price']] + [row[sv] for sv in sales_versions['SalesVersion']])
-        # format the cells
-        for cell in ws[len(ws["A"])]:
-            cell.border = all_border
-
-def prepare_sheet(ws, df_sales_versions, title):
-    
-    # Content of Row 1 & 2
-    ws.sheet_view.showGridLines = False
-    ws.freeze_panes = ws['A2']
-    ws.merge_cells('A1:B1')
-    ws['A1'] = f'{title} - Polster & Farben'
-    ws['C1'] = 'EUR inkl. 19 % MwSt.\n EUR ohne MwSt.'
-    ws['A2'] = 'Code (ab Werk)\n + VCG Paket'
-    ws['B2'] = 'Beschreibung'
-
-    for idx, row in df_sales_versions.iterrows():
-        ws.cell(row=1, column=idx+4, value=f'{row["SalesVersionName"]}\nSV {row["SalesVersion"]}')
-
-    max_r_pre = ws.max_row 
-
-    for row in range(max_r_pre, 3, -1):  # Beginne von unten, um die Position der Zeilen nicht zu verändern
-        ws.insert_rows(row)
-
-    row = 3
-    while row <= max_r_pre:
-    # Test if cell in column C has content
-        if ws.cell(row=row, column=3).value:
-        # Split up price data in column C
-            prices = ws.cell(row=row, column=3).value.split('/')
-        
-        # Move second price into next row
-            if len(prices) > 1:
-                next_empty_row = row + 1
-                while ws.cell(row=next_empty_row, column=3).value:
-                    next_empty_row += 1
-                ws.cell(row=next_empty_row, column=3).value = float(prices[1])
-                ws.cell(row=row, column=3).value = float(prices[0])
-                max_r_pre += 1  # One extra row has been added, so increase max_r
-            row += 2  # Skip next row since it was added just now
-        else:
-            row += 1
-
-
-    #Definition of column widths & row heights
-    max_c = ws.max_column
-    max_r = ws.max_row 
-
-    ws.column_dimensions['A'].width = 11
+    ws.column_dimensions['A'].width = 10
     ws.column_dimensions['B'].width = 65
-    for col in range(3, max_c + 1):
-        ws.column_dimensions[get_column_letter(col)].width = 25
+    for col in range(3, ws.max_column+1):
+        ws.column_dimensions[ws.cell(row=1, column=col).column_letter].width = 20
 
-    ws.row_dimensions[1].height = 45
-    ws.row_dimensions[2].height = 35
+    ws.column_dimensions[ws.cell(row=1, column=ws.max_column-1).column_letter].width = 1
+    for cell in ws[ws.cell(row=1, column=ws.max_column-1).column_letter]:
+        cell.fill = PatternFill(start_color='FFFFFF', end_color='FFFFFF', fill_type='solid')
+        if cell.row == 2*len(df_upholstery) + 1:
+            cell.border = Border(top=None, bottom=None, left=None, right=None)
+        else:
+            cell.border = Border(top=None, bottom=None, left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'))
 
-    # Formatting of first row
-    ws['A1'].font = Font(name='Arial', size=16, bold=True, color="FFFFFF")
-    ws['A1'].alignment = Alignment(horizontal='left', vertical='center')
     
-    for col in range(3, 20):
-        cell = ws.cell(row=1, column=col)
+    ws.sheet_view.showGridLines = False
+
+def insert_table(ws, sales_versions, title, df_res):
+
+    insert_title(ws, sales_versions, title)
+    for _, row in df_res.iterrows():
+        svs = [cell_values.get(row[sv], row[sv]) for sv in sales_versions['SalesVersion']]
+        if row['Price'] == 'Pack Only'or row['Price'] == 'Serie':
+            ws.append([row['Code'], row['CustomName'], row['Price']] + svs + ['', row['Rule']])
+            ws.append([])
+        else:
+            prices = row['Price'].split('/')
+            if len(prices) > 1:
+                ws.append([row['Code'], row['CustomName'], prices[0]] + svs + ['', row['Rule']])
+                ws.cell(row=ws.max_row, column=3).alignment = Alignment(horizontal='center', vertical='bottom')
+                ws.append(['', '', prices[1]] + svs) 
+                ws.cell(row=ws.max_row, column=3).alignment = Alignment(horizontal='center', vertical='top')
+        foramt_row(ws, ws.max_row-1, ws.max_row)
+    format_first_two_columns(ws, ws.max_row - len(df_res), ws.max_row)
+
+def format_first_two_columns(ws, row1, row2):
+    # for each row in the first column, merge the cells in the first two columns if the cell's value is the same as the one above
+    for row in range(row1, row2+1):
+        if row == row1:
+            continue
+        if ws.cell(row=row, column=1).value == ws.cell(row=row-1, column=1).value:
+            ws.merge_cells(f'A{row-1}:A{row}')
+            ws.merge_cells(f'B{row-1}:B{row}')
+    
+
+def foramt_row(ws, row1, row2):
+    for col in range(1, ws.max_column+1):
+        if col != 3:
+            cell = ws.cell(row=row1, column=col)
+            alig = 'center' if col != 2 else 'left'
+            cell.alignment = Alignment(horizontal=alig, vertical='center', wrap_text=True)
+            cell.border = all_border
+            cell.font = Font(name='Arial', size=10)
+            ws.merge_cells(f'{ws.cell(row=row1, column=col).column_letter}{row1}:{ws.cell(row=row2, column=col).column_letter}{row2}')
+        else:
+            # set alignemnt to bottom for the first row and top for the second row
+            # remove the upper border from the second row
+            ws.cell(row=row1, column=col).alignment = Alignment(horizontal='center', vertical='bottom')
+            ws.cell(row=row1, column=col).font = Font(name='Arial', size=10, bold=True)
+            ws.cell(row=row2, column=col).alignment = Alignment(horizontal='center', vertical='top')
+            ws.cell(row=row2, column=col).border = Border(top=None, bottom=Side(style='thin', color='000000'), left=Side(style='thin', color='000000'), right=Side(style='thin', color='000000'))
+        
+
+def insert_title(ws, sales_versions, title):
+
+    # Content of Row 1 & 2
+    ws.append([title, '', 'EUR inkl. 19 % MwSt.\n EUR ohne MwSt.'] + [f'{row["SalesVersionName"]}\nSV {row["SalesVersion"]}' for _, row in sales_versions.iterrows()] + ['', 'Bemerkungen'])
+    insert_row_index = ws.max_row
+    ws.merge_cells(f'A{insert_row_index}:B{insert_row_index}')
+    
+    # fill color
+    for col in range(1, ws.max_column+1):
+        cell = ws.cell(row=insert_row_index, column=col)
+        cell.fill = fill
         cell.font = Font(name='Arial', size=10, bold=True, color='FFFFFF')
         cell.alignment = Alignment(horizontal='center', vertical='center',wrap_text=True)
-
-    for col in range(1,max_c+1):
-        cell = ws.cell(row=1, column=col)
-        cell.fill = fill
-        
-    # Formatting of second row
-    ws['A2'].alignment = Alignment(horizontal='center', vertical='center',wrap_text=True)
-    ws['A2'].font = Font(size=10, bold=True)
-    ws['B2'].alignment = Alignment(horizontal='center', vertical='center')
-    ws['B2'].font = Font(size=10, bold=True)
-    
-
-    # Formatting of content tables
-    target_row = None
-    for row in range(1, max_r):
-        if ws.cell(row=row, column=1).value == "Außenfarben":
-            target_row = row - 3  # Zwei Zeilen darüber
-            break
-    
-    if target_row is not None:
-        # Formatierung der Zellen im Bereich zwischen Zeile 3 und target_row
-        for row in range(3, target_row + 1):
-            for col in range(1, max_c + 1):
-                cell = ws.cell(row=row, column=col)
-                cell.border = Border(left=Side(style='thin'),
-                                    right=Side(style='thin'),
-                                    top=Side(style='thin'),
-                                    bottom=Side(style='thin'))
-
-        for row in range(3, target_row, 2):
-            for col in range(1, 3):
-                ws.merge_cells(start_row=row, start_column=col, end_row=row+1, end_column=col)
-            for col in range(1, 2):
-                cell = ws.cell(row=row, column=col)
-                cell.alignment = Alignment(horizontal='right', vertical='center')
-            for col in range(2, 3):
-                cell = ws.cell(row=row, column=col)
-                cell.alignment = Alignment(horizontal='left', vertical='center')
-            for col in range(4, max_c + 1):
-                ws.merge_cells(start_row=row, start_column=col, end_row=row+1, end_column=col)
-                cell = ws.cell(row=row, column=col)
-                cell.alignment = Alignment(horizontal='center', vertical='center')
-    
-        # Formatting of second table
-        for row in range(1, ws.max_row):
-            if ws.cell(row=row, column=1).value == 'Außenfarben':
-                for column in range(1, ws.max_column):
-                    cell = ws.cell(row=row, column=column)
-                    cell.font = Font(name='Arial', size=16, bold=True, color="FFFFFF")
-                    cell.fill = fill
-                break
-    
-        for row in range(target_row + 1 , max_r, 2):
-            for col in range(1, 3):
-                ws.merge_cells(start_row=row, start_column=col, end_row=row+1, end_column=col)
-            for col in range(1, 2):
-                cell = ws.cell(row=row, column=col)
-                cell.alignment = Alignment(horizontal='right', vertical='center')
-            for col in range(2, 3):
-                cell = ws.cell(row=row, column=col)
-                cell.alignment = Alignment(horizontal='left', vertical='center')
-            for col in range(4, max_c + 1):
-                ws.merge_cells(start_row=row, start_column=col, end_row=row+1, end_column=col)
-                cell = ws.cell(row=row, column=col)
-                cell.alignment = Alignment(horizontal='center', vertical='center')
 
 def fetch_color_data(sales_versions, time):
     df_pno_color = DBOperations.instance.get_table_df(DBOperations.instance.config.get('AUTH', 'COL'), columns=['ID', 'PNOID', 'Code', 'RuleName', 'StartDate', 'EndDate'])
@@ -210,6 +125,7 @@ def fetch_color_data(sales_versions, time):
     df_pno_color = df_pno_color.merge(sales_versions[['TmpCode', 'SalesVersion', 'SalesVersionName']], left_on='PNOID', right_on='TmpCode', how='left')
     df_pno_color.drop(columns='TmpCode', inplace=True)
     df_pno_color_with_sv = df_pno_color[df_pno_color['SalesVersion'].notna()]
+
     # Replace ID with Price from df_pno_color_price
     df_pno_color_with_price = df_pno_color_with_sv.merge(df_pno_color_price, left_on='ID', right_on='RelationID', how='left')
 
@@ -230,6 +146,8 @@ def fetch_color_data(sales_versions, time):
     # Join the pivoted DataFrame with the original one. sort after code ascending
     df_result = df_pno_color_with_price.join(pivot_df, on=['Code', 'Price']).sort_values(by='Code')
 
+    df_result['Rule'] = 'Rule'
+
     return df_result
 
 def fetch_upholstery_data(sales_versions, time):
@@ -241,6 +159,7 @@ def fetch_upholstery_data(sales_versions, time):
     df_pno_upholstery = df_pno_upholstery.merge(sales_versions[['TmpCode', 'SalesVersion', 'SalesVersionName']], left_on='PNOID', right_on='TmpCode', how='left')
     df_pno_upholstery.drop(columns='TmpCode', inplace=True)
     df_pno_upholstery_with_sv = df_pno_upholstery[df_pno_upholstery['SalesVersion'].notna()]
+
     # Replace ID with Price from df_pno_upholstery_price
     df_pno_upholstery_with_price = df_pno_upholstery_with_sv.merge(df_pno_upholstery_price, left_on='ID', right_on='RelationID', how='left')
 
@@ -261,4 +180,10 @@ def fetch_upholstery_data(sales_versions, time):
     # Join the pivoted DataFrame with the original one. sort after code ascending
     df_result = df_pno_upholstery_with_price.join(pivot_df, on=['Code', 'Price']).sort_values(by='Code')
 
+    df_result['Rule'] = 'Rule'
+
     return df_result
+
+def get_series_features_per_option(df, conditions):
+    df_pno_features = DBOperations.instance.get_table_df(DBOperations.instance.config.get('AUTH', 'FEAT'), columns=['PNOID', 'Reference', 'RuleName as FeatRule', 'CustomName as FeatName', 'CustomCategory'], conditions=pno_features_conditions)
+    df_pno_features.drop_duplicates(inplace=True)
