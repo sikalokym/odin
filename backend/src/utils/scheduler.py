90c29c71c7247d367cf271bcc5e1016db7df2885
Hassan Wahba <hassan.wahba@volvocars.com>
2024-06-11 11:18:29 +0200

    create db connection and remove it in the scheduler thread when triggered

--- backend/src/utils/scheduler.py
+++ backend/src/utils/scheduler.py
@@ -1,3 +1,5 @@
+from src.database.db_connection import DatabaseConnection
+from src.database.db_operations import DBOperations
 from src.ingest.cpam.services import get_supported_countries, ingest_all_cpam_data
 from src.utils.sql_logging_handler import logger
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -7,9 +9,13 @@ import atexit
 import os
 
 
-def scheduled_task(country):
+def scheduled_task():
     try:
-        ingest_all_cpam_data(country)
+        DBOperations.create_instance(logger=logger)
+        countries = get_supported_countries()
+        for country in countries:
+            ingest_all_cpam_data(country)
+        DatabaseConnection.close_connection()
     except Exception as e:
         logger.error(f"Failed to complete scheduled task: {e}", extra={'country_code': country})
 
@@ -19,22 +25,17 @@ config.read('config/cpam.cfg')
 env_var_name = config.get('REFRESH_DATE', 'ENV_VAR_NAME')
 separator = config.get('REFRESH_DATE', 'ENV_VAR_NAME_SEPARATOR')
 date = os.getenv('env_var_name', f'mon{separator}9')
-minute = 1
-job_duration = 20
 
 try:
     day, hour = date.split(separator)
     hour = int(hour)
 except Exception as e:
-    logger.error(f"Failed to parse the refresh date: {e}", extra={'country_code': 231})
+    logger.error(f"Failed to parse the refresh date: {e}", extra={'country_code': 'All'})
     day, hour = 'mon', 9
 
 cpam_scheduler = BackgroundScheduler(daemon=True, timezone=utc)
 
-countries = get_supported_countries()
-for country in countries:
-    cpam_scheduler.add_job(scheduled_task, 'cron', day_of_week=day, hour=hour, minute=minute, args=[country], id=f'cpam_{country}', replace_existing=True)
-    minute += job_duration
+cpam_scheduler.add_job(scheduled_task, 'cron', day_of_week=day, hour=hour, minute=3)
 
 # Shut down the scheduler when exiting the app
 atexit.register(lambda: cpam_scheduler.shutdown())
