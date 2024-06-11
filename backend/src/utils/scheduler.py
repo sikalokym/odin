d2f0379ff63156408be626f1f1a54713ff263837
Hassan Wahba <hassan.wahba@volvocars.com>
2024-06-11 09:19:41 +0200

    Update scheduler to handle multiple countries in background tasks and trigger on monday instead of sunday

--- backend/src/utils/scheduler.py
+++ backend/src/utils/scheduler.py
@@ -1,32 +1,40 @@
 from src.ingest.cpam.services import get_supported_countries, ingest_all_cpam_data
+from src.utils.sql_logging_handler import logger
 from apscheduler.schedulers.background import BackgroundScheduler
 from pytz import utc
 import configparser
-import logging
 import atexit
 import os
 
 
-def scheduled_task():
+def scheduled_task(country):
     try:
-        # Call the function directly with the desired country parameter
-        countries = get_supported_countries()
-        for country in countries['CountryName']:
-            ingest_all_cpam_data(country)
+        ingest_all_cpam_data(country)
     except Exception as e:
-        logging.error(f"Failed to complete scheduled task: {e}")
+        logger.error(f"Failed to complete scheduled task: {e}", extra={'country_code': country})
 
 config = configparser.ConfigParser()
 config.read('config/cpam.cfg')
 
 env_var_name = config.get('REFRESH_DATE', 'ENV_VAR_NAME')
 separator = config.get('REFRESH_DATE', 'ENV_VAR_NAME_SEPARATOR')
-date = os.getenv('env_var_name', f'sun{separator}12')
-day, hour = date.split(separator)
-hour = int(hour)
+date = os.getenv('env_var_name', f'mon{separator}9')
+minute = 1
+job_duration = 20
+
+try:
+    day, hour = date.split(separator)
+    hour = int(hour)
+except Exception as e:
+    logger.error(f"Failed to parse the refresh date: {e}", extra={'country_code': 231})
+    day, hour = 'mon', 9
 
 cpam_scheduler = BackgroundScheduler(daemon=True, timezone=utc)
-cpam_scheduler.add_job(scheduled_task, 'cron', day_of_week='sun', hour=12, minute=0)
+
+countries = get_supported_countries()
+for country in countries:
+    cpam_scheduler.add_job(scheduled_task, 'cron', day_of_week=day, hour=hour, minute=minute, args=[country], id=f'cpam_{country}', replace_existing=True)
+    minute += job_duration
 
 # Shut down the scheduler when exiting the app
 atexit.register(lambda: cpam_scheduler.shutdown())
