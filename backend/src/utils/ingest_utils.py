c8897dfd416baf5ec6329b59943b60e1d3eb0ad1
Hassan Wahba <hassan.wahba@volvocars.com>
2024-06-14 09:37:48 +0200

    handle CPAM not available case

--- backend/src/utils/ingest_utils.py
+++ backend/src/utils/ingest_utils.py
@@ -13,7 +13,9 @@ def is_valid_year(year, country_code):
         bool: True if year is valid, False otherwise
     """
     years = get_model_years(country_code)
-    return year in years['Years'] if years else 'CPAM internal error'
+    if not years or years == []:
+        return 'CPAM internal error'
+    return year in years['Years']
 
 def is_valid_car_type(car_type, year, country_code):
     """Check if car type is valid.
@@ -27,7 +29,9 @@ def is_valid_car_type(car_type, year, country_code):
     """
     car_types = get_car_types(year, country_code)
     
-    return car_type in [car['Type'] for car in car_types['DataRows']] if car_types else 'CPAM internal error'
+    if not car_types or car_types == []:
+        return 'CPAM internal error'
+    return car_type in [car['Type'] for car in car_types['DataRows']]
 
 def is_valid_engine_category(engine_category, year, country_code, model):
     """Check if engine category is valid.
