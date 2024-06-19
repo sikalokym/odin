93ade568d79e8f571f50d7ed71272ff97e8899c7
Hassan Wahba <hassan.wahba@volvocars.com>
2024-06-19 14:53:21 +0200

    fix fetching condition for engine cats

--- backend/src/database/services.py
+++ backend/src/database/services.py
@@ -5,10 +5,10 @@ from src.utils.db_utils import get_model_year_from_date
 def get_engine_cats(country, model_year, model):
     conditions = [f'CountryCode = {country}']
     
-    if model:
-        conditions.append(f"Model = '{model}'")
     engines = DBOperations.instance.get_table_df(DBOperations.instance.config.get('TABLES', 'En'), columns=['Code', 'EngineCategory'], conditions=conditions)
 
+    if model:
+        conditions.append(f"Model = '{model}'")
     pno_engins = DBOperations.instance.get_table_df(DBOperations.instance.config.get('AUTH', 'PNO'), columns=['Engine', 'StartDate'], conditions=conditions)
     
     pno_engins = DBOperations.instance.get_table_df(DBOperations.instance.config.get('AUTH', 'PNO'), columns=['Engine', 'StartDate'], conditions=conditions)
