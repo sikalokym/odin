966e7576d8dc86956a7d61a7911f8f2241bb9a1c
Hassan Wahba <hassan.wahba@volvocars.com>
2024-06-25 18:05:10 +0200

    chore: Update get_engine_cats function to filter out empty EngineCategory values

--- backend/src/database/services.py
+++ backend/src/database/services.py
@@ -3,7 +3,7 @@ from src.utils.db_utils import get_model_year_from_date
 
 
 def get_engine_cats(country, model_year, model):
-    conditions = [f'CountryCode = {country}']
+    conditions = [f'CountryCode = {country}', 'EngineCategory IS NOT NULL', 'EngineCategory != ""']
     
     engines = DBOperations.instance.get_table_df(DBOperations.instance.config.get('TABLES', 'En'), columns=['Code', 'EngineCategory'], conditions=conditions)
 
