bea8a48bdd57fc757cebc0d921952be0efad0b03
Hassan Wahba <hassan.wahba@volvocars.com>
2024-07-22 10:37:08 +0200

    chore: Update author name in code files

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import { initializeMsal } from './authConfig';
 import { ensureAuthenticated } from './authService';
 
+// Author: Hassan Wahba
 
 const pinia = createPinia()
 
