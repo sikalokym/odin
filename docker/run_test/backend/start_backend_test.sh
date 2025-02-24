adf10fd377ec14ad44bf0dee913dfdbf20966780
Kis <MKIS@volvocars.com>
2025-02-24 21:58:58 +0100

    backend testing

--- docker/run_test/backend/start_backend_test.sh
+++ docker/run_test/backend/start_backend_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # export DB_CONNECTION_STRING=${DB_CONNECTION_STRING}Pwd=$(cat /run/secrets/sa_password);
 pip3 install -r /app/requirements.txt -r /app/requirements_dev.txt
-sqlcmd -S test_odin_database -U $SQL_DB_UID -P $SQL_DB_PWD -C -d odin -i /odin_database/dbo.SupportedCountry.Table.sql
+/opt/mssql-tools18/bin/sqlcmd -S test_odin_database -U $SQL_DB_UID -P $SQL_DB_PWD -C -d odin -i /odin_database/dbo.SupportedCountry.Table.sql
+cd /app
 pytest -s -vv
