c953a4588ab9f3c5b0bd350497d52e9e8e2d93ba
Kis <MKIS@volvocars.com>
2025-02-25 20:03:33 +0100

    added test visa files

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -13,3 +13,10 @@ def test_supported_countries(client, mocker):
     response = client.get("/api/setup/supported_countries")
     assert response.status_code == 200
     assert response.json == expected_responce
+
+
+def test_visa_files(client, mocker):
+    expected_responce = '[{"VisaFile":"VISA C40 MY25_24w17 (24w05) .xlsx","CarType":"539","CustomName":"EC40"}]'
+    response = client.get("/api/db/231/2025/visa-files")
+    assert response.status_code == 200
+    assert response.text == expected_responce
